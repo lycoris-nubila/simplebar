2a0ff83d406b8a6f392d1ff8c0f66673531200bd
Sébastien Doubey <sebastien.doubey@gmail.com>
2020-04-28 16:21:53 +0200

    Add cdk scroll directive

--- packages/simplebar-angular/src/lib/simplebar-angular.module.ts
+++ packages/simplebar-angular/src/lib/simplebar-angular.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SimplebarAngularComponent } from './simplebar-angular.component';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [SimplebarAngularComponent],
-  imports: [],
+  imports: [ScrollingModule],
   exports: [SimplebarAngularComponent]
 })
 export class SimplebarAngularModule {}
